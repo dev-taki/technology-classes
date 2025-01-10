9dcc2dcb9d8bda39f6e0cef1d44f5444c1d09349
Md Sajjad Bhuiyan Taki <taki@Mds-MacBook-Air.local>
2025-01-10 12:33:17 +0000

    learn with takii and takii.dev

--- app/page.js
+++ app/page.js
@@ -63,7 +63,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          Learn with Takii
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -93,7 +93,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Go to takii.dev →
         </a>
       </footer>
     </div>
